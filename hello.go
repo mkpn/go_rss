0306d8c43018a3d97ac7146cab504d1da433faad
makoto-yoshida <makoto.yoshida@dena.com>
2018-03-05 12:51:59 +0900

    rssフィード取得のためのリクエストっぽいの書いた

--- hello.go
+++ hello.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"html"
-	"net/http"
+    "fmt"
+    "html"
+    "net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request, response http.Response) {
+    fmt.Fprintf(w, "これはテスト〜, %q", html.EscapeString(r.URL.Path))
 }
 
 func main() {
-	http.HandleFunc("/hello", handler)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/hello", handler)
+    http.ListenAndServe(":8080", nil)
+    response, err := http.Get("http://toyokeizai.net/list/feed/rss")
 }
+
