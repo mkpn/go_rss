ef2a202218368b1808f821f70703483c3e9ecbab
makoto-yoshida <makoto.yoshida@dena.com>
2018-03-05 19:45:18 +0900

    rssリーダーできた

--- hello.go
+++ hello.go
@@ -4,19 +4,25 @@ import (
     "fmt"
     "html"
     "net/http"
+    "io/ioutil"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
-func testHandler(w http.ResponseWriter, r *http.Request, response http.Response) {
-    fmt.Fprintf(w, "これはテスト〜, %q", html.EscapeString(r.URL.Path))
+func testHandler(w http.ResponseWriter, r *http.Request) {
+    response, err := http.Get("http://toyokeizai.net/list/feed/rss")
+    if err != nil {
+        return
+    }
+    bodyBytes, err := ioutil.ReadAll(response.Body)
+    fmt.Fprintf(w, "これはテスト〜, %q", string(bodyBytes))
 }
 
 func main() {
     http.HandleFunc("/hello", handler)
+    http.HandleFunc("/test", testHandler)
     http.ListenAndServe(":8080", nil)
-    response, err := http.Get("http://toyokeizai.net/list/feed/rss")
 }
 
