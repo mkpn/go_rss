f110df4f5e4aabfb87fb07371bce33019e9e85d5
neesan <exp.ym.lvl@gmail.com>
2018-03-07 00:56:31 +0900

    packagingの仕方ﾜｯｶﾝﾈ

---
+++ feedxml.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"encoding/xml"
+)
+
+type Xml struct {
+	Articles []struct { //これなんで入れ子(内部クラス？)にする必要あるんだ？
+		Title       string `xml:"title"`
+		Link        string `xml:"link"`
+		Description string `xml:"description"`
+		// パース前とパース後で全然違うフォーマットになってるのｲﾐﾌﾜﾛﾀ
+		// あと元のデータpubDateってなってるのにdateじゃないとアクセスできなかったのもｲﾐﾌ
+		Date string `xml:"date"`
+	} `xml:"item"`
+}
+
+func GetFeedArticles(url string) (Xml, error) { //返り値の構文ミス補完してくれなかった。
+	response, _ := http.Get(url)
+	bodyBytes, _ := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+
+	feedXmlStr := string(bodyBytes)
+	feedXml := Xml{}
+	err := xml.Unmarshal([]byte(feedXmlStr), &feedXml)
+	return feedXml, err
+}
---
+++ rssarticles.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"time"
+)
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	xml, err := GetFeedArticles("http://b.hatena.ne.jp/hotentry/it.rss")
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	for _, articles := range xml.Articles {
+		// timeParseの規格の選びやすさ良き〜〜〜
+		datetime, _ := time.Parse(time.RFC3339, articles.Date)
+
+		fmt.Fprintf(w, "%v\n", datetime.Format("2006/01/02 15:04:05"))
+		fmt.Fprintf(w, "%s\n", articles.Title)
+		fmt.Fprintf(w, "%v\n", articles.Link)
+		fmt.Fprintln(w)
+	}
+}
